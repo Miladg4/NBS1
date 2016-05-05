8979f23b81fed60784fe24504b3e045660265858
Miladg4 <miladg@gmail.com>
2016-05-04 19:18:24 -0700

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include <fstream>
 #include <exception>
 #include <string>
-
+#include <vector>
 
 
 using namespace std;
@@ -28,37 +28,80 @@ int main(int argc, const char * argv[]) {
 
     //    char dir[PATH_MAX];
     string fileName = "a";
+    bool flag = false;
     string line = "";
-    int mapReadCount = 0;
+    //int mapReadCount = 0;
     ofstream outfile;
     outfile.open ("example.txt");
     outfile << "Writing this to a file.\n";
     outfile.close();
+    string result[100][3];
+    vector< vector <string> > twoDimVec;
+    vector<string> row;
     
     ifstream myFile;
     myFile.open("a.txt");
-//    cout << "Please input the file name:";
-//    cin >> fileName;
-//    getcwd(dir, PATH_MAX);
-//    string temp(dir);
-//    cout << temp << " " << temp.size();
-
+    int c = 0;
+    int j = 0;
+    string emp = "";
     if (myFile.is_open())
     {
         while (!myFile.eof())
         {
             getline(myFile, line);
+
             char temp [line.size()];
-            for (unsigned i=0; i < line.size(); i++){
-                temp[i] = line[i];
-            }
-            if (temp[0] != '@') {
-                char* token = strtok(temp, " ");
-                while (token != nullptr)
+            memset(&temp[0], 0, sizeof(temp));          //clearing the char
+            
+            
+            if (line[0] != '@' && line.size() != 0)
+            {
+                unsigned i=0;
+                for (; i < line.size(); i++)
                 {
-                    token = strtok(nullptr, " ");
+                    temp[i] = line[i];
                 }
 
+                char* token;
+                if (j == 0 )
+                {
+                    token = strtok(temp, " ");
+                    for (i = 0; token[i] != 0; i++);
+                    result[c][j] = token;
+                    row.push_back(token);
+                    if (line.size() != i)
+                        line.erase(0, i+1);
+                    else if (line.size() == i)
+                        line.erase();
+                }
+                
+                while (j < 3)
+                {
+                    token = strtok(nullptr, " ");
+                    if (line == "")
+                        break;
+                    result[c][j] = token;
+                    row.push_back(token);       //adding the coloumn
+                    j++;
+                    if (line.size() != i)
+                        line.erase(0, i+1);
+                    else if (line.size() == i)
+                        line.erase();
+                }
+            }
+            else if (line[0] == '@')
+            {
+                cout << "skiping this line" << endl;
+                flag = true;
+            }
+            
+            if (!flag)
+            {
+                twoDimVec.push_back(row);
+                row.clear();
+                j = 0;
+                c++;
+                flag = false;
             }
         }
         
@@ -66,7 +109,18 @@ int main(int argc, const char * argv[]) {
     else
         cerr << "Couldn't open the file: " << fileName.c_str() << endl;
         //throw exception(string("Couldn't open the file: " + fileName).c_str());
+
     myFile.close();
 
+    cout << "vector result: " << endl;
+
+    cout<< twoDimVec[0][0] << endl;
+    cout<< twoDimVec[0][1] << endl;
+    cout<< twoDimVec[0][2] << endl;
+    
+    cout<< twoDimVec[1][0] << endl;
+    cout<< twoDimVec[1][1] << endl;
+    cout<< twoDimVec[1][2] << endl;
+    
     return 0;
 }
